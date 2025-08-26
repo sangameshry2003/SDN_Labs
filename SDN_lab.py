bb4fb4f4653d3d2c629e7518e564dcc372aa4433
Sangamesh Ramesh Yankanchi <131347663+sangameshry2003@users.noreply.github.com>
2025-08-26 22:37:31 +0530

    Update SDN_lab.py

--- SDN_lab.py
+++ SDN_lab.py
@@ -6,7 +6,7 @@ from sklearn.metrics import accuracy_score
 # Load dataset
 iris = load_iris()
 X_train, X_test, y_train, y_test = train_test_split(
-    iris.data, iris.target, test_size=0.3, random_state=42
+    iris.data, iris.target, test_size=0.2, random_state=42
 )
 
 # Train model
